586027055c29852283deecf2a9f6cc153c68a4cb
Sean Yang <seanyixuan@gmail.com>
2021-10-30 16:54:57 -0700

    got general styling down for tablet view

--- src/components/CommentForm/CommentForm.js
+++ src/components/CommentForm/CommentForm.js
@@ -3,13 +3,15 @@ import "./CommentForm.scss";
 import uploadButton from '../../assets/icons/upload.svg';
 
 export default function CommentForm() {
-    return(
+    return (
    
         <div className="comments__divider">
             <figure className="comments__default-avatar"></figure>
             <form name="form" className="form" id="form" method="post">
-                <label for="comment">Join the Conversation</label>
-                <textarea id="comment-box" name="comment-box" placeholder="Add a new comment"></textarea>
+                <div className="comments__label-textarea">
+                    <label for="comment">Join the Conversation</label>
+                    <textarea id="comment-box" name="comment-box" placeholder="Add a new comment"></textarea>
+                </div>
                 <button type="submit" className="header__button" id="addComment" value="Add Comment" src={uploadButton}>
                     <img className="header__button-icon" src={uploadButton}/>
                     <h2 className="header__button-text">COMMENT</h2>
