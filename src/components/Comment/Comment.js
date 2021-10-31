33b29b34c48df92f4b3249266d23d83a13ea9ac0
Sean Yang <seanyixuan@gmail.com>
2021-10-30 19:27:57 -0700

    formatted timestamp to proper dates

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -3,13 +3,23 @@ import "./Comment.scss";
 
 export default function Comment(props) {
     console.log(props.data);
+
+    // creating timestamp
+    let timeStamp = props.data.timestamp;
+    let date = new Date(timeStamp * 1.00001);
+    let dateString = date.toLocaleString().split(',')[0];
+    console.log(dateString);
+    
+
+
+
     return (
       <div className="comment">
         <figure className="comment__avatar"></figure>
         <div className="comment__content">
           <div className="comment__content-top">
             <p className="comment__name">{props.data.name}</p>
-            <p className="comment__date">{props.data.timestamp}</p>
+            <p className="comment__date">{dateString}</p>
           </div>
           <p className="comment__text">{props.data.comment}</p>
         </div>
--- src/components/Video Description/VideoDescription.js
+++ src/components/Video Description/VideoDescription.js
@@ -5,13 +5,24 @@ import LikesIcon from "../../assets/icons/likes.svg";
 
 export default function VideoDescription(props) {
   console.log(props.data);
+
+    // creating timestamp 
+
+    let timeStamp = props.data.timestamp;
+    let date = new Date(timeStamp * 1.00001);
+    let dateString = date.toLocaleString().split(',')[0];
+    console.log(dateString);
+ 
+
+
+
   return (
     <section className="videodescription">
       <h1 className="videodescription__heading">{props.data.title}</h1>
       <div className="videodescription__subtext">
         <div className="videodescription__by-timestamp">
           <h2 className="videodescription__by">{`By ${props.data.channel}`}</h2>
-          <p className="videodescription__timestamp">{props.data.timestamp}</p>
+          <p className="videodescription__timestamp">{dateString}</p>
         </div>
         <div className="videodescription__icons-container">
           <div className="videodescription__icons-views">
