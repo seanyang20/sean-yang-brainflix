71a793ccee81de19013636cc74d6e2355af0b4d0
Sean Yang <seanyixuan@gmail.com>
2021-11-09 17:36:58 -0800

    got /videos and /:id route working

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -22,18 +22,35 @@ const getVideoData = () => {
 
   // console.log(getVideoData());
 getVideoData();
-console.log(videos);
+// console.log(videos);
 
-router.get("/videos", (req, res) => {               
+router.get("/", (req, res) => {               
     console.log('GET method called');
-    console.log(getVideoData());
+    // console.log(getVideoData());
     res.json(videos);
     // console.log(res);
     // console.log(req);
     })
 
-router.get("/videos/:id", (req, res) => {
-    console.log(req.params);
+router.get("/:id", (req, res) => {
+    // console.log(videos);
+    const singleVideo = videos.find((vid) => {
+        return vid.id === req.params.id;
     })
+    console.log(singleVideo);
+    if (singleVideo) {
+        res.json(singleVideo);
+    } else {
+        res.status(404).send("We can't find that shoe.");
+    }
+    // const individualShoe = shoeData.find((shoe) => {
+    //     return shoe.id === req.params.id;
+    // });
+    // if (individualShoe) {
+    //     res.json(individualShoe);
+    // } else {
+    //     res.status(404).send("We can't find that shoe.");
+    // }
+    });
 
 module.exports = router;
