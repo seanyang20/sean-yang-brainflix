ae735ddcf3834ca59240138e6e5579dd0fead5b4
Sean Yang <seanyixuan@gmail.com>
2021-11-10 16:41:51 -0800

    got post route to work

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const fs = require("fs");
 
 
-
-let videos = [];
+let videoData = [];
 
 const getVideoData = () => {
     console.log("getVideoData is working");
@@ -14,43 +13,54 @@ const getVideoData = () => {
         return;
         
       }
-      videos = JSON.parse(data);
-    //   console.log(videos);
+      videoData = JSON.parse(data);
+   
     });
-    // console.log(videos, data);
+  
   };
 
-  // console.log(getVideoData());
+  
 getVideoData();
-// console.log(videos);
+
 
 router.get("/", (req, res) => {               
     console.log('GET method called');
-    // console.log(getVideoData());
-    res.json(videos);
-    // console.log(res);
-    // console.log(req);
+    res.json(videoData);
+ 
     })
 
 router.get("/:id", (req, res) => {
-    // console.log(videos);
-    const singleVideo = videos.find((vid) => {
+    console.log('GET videos/:id method called');
+    const singleVideo = videoData.find((vid) => {
         return vid.id === req.params.id;
     })
-    console.log(singleVideo);
+    // console.log(singleVideo);
     if (singleVideo) {
         res.json(singleVideo);
     } else {
         res.status(404).send("We can't find that shoe.");
     }
-    // const individualShoe = shoeData.find((shoe) => {
-    //     return shoe.id === req.params.id;
-    // });
-    // if (individualShoe) {
-    //     res.json(individualShoe);
-    // } else {
-    //     res.status(404).send("We can't find that shoe.");
-    // }
     });
+
+router.post("/", (req, res) => {
+    console.log('POST videos/ method called');
+    let videos = videoData;
+    const {channel, description, id} = req.body;
+    console.log(req.body);
+    if (!channel || !description || !id) {
+        res.status(400).send("Please supply channel and description properties in your POST request");
+    }
+
+    const newVideoObj = { id, channel, description };
+    videos.push(newVideoObj);
+    fs.writeFile('./data/videos.json', 
+    JSON.stringify(videos), (err) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+        console.log("File written successfully!");
+        res.status(201).json(newVideoObj);
+    })
+}) 
 
 module.exports = router;
