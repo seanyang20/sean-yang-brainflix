40b24a45088d6eb97e8a4a7afc6c2f5f657bb411
Sean Yang <seanyixuan@gmail.com>
2021-11-10 18:08:34 -0800

    got uniqid to work

--- server/index.js
+++ server/index.js
@@ -6,46 +6,22 @@ const cors = require('cors');
 const morgan = require('morgan');
 const path = require("path");
 const videosJS = require("./routes/videos.js");
+const axios = require('axios');
 
+// Configuration
+require('dotenv').config();
+const port = process.env.PORT || 8080;
 
+// Middleware
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, "public")));
 app.use(morgan('tiny'));
 
-// let videos = [];
 
-// const getVideoData = () => {
-//     console.log("getVideoData is working");
-//     fs.readFile("./data/videos.json", (err, data) => {
-//       if (err) {
-//         console.log(err);
-//         return;
-//       }
-//       videos = JSON.parse(data);
-//       // console.log(videos);
-//     });
-//     // console.log(videos, data);
-//   };
-
-// // console.log(getVideoData());
-// getVideoData();
-// console.log(videos);
+app.use(express.static(path.join(__dirname, "public")));
 
 
-// app.get("/videos", (req, res) => {               
-// console.log('GET method called');
-// console.log(getVideoData());
-// res.json(videos);
-// // console.log(res);
-// // console.log(req);
-// })
-
-// app.get("/videos/:id", (req, res) => {
-//   console.log(req);
-// })
-
-app.use("/videos", videosJS);
+app.use("/", videosJS);
 
 app.listen(PORT, () => {
     console.log(`Express server is up and running on Port ${PORT}!`);
--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 
 let videoData = [];
@@ -23,13 +24,13 @@ const getVideoData = () => {
 getVideoData();
 
 
-router.get("/", (req, res) => {               
+router.get("/videos", (req, res) => {               
     console.log('GET method called');
     res.json(videoData);
  
     })
 
-router.get("/:id", (req, res) => {
+router.get("/videos/:id", (req, res) => {
     console.log('GET videos/:id method called');
     const singleVideo = videoData.find((vid) => {
         return vid.id === req.params.id;
@@ -42,25 +43,45 @@ router.get("/:id", (req, res) => {
     }
     });
 
-router.post("/", (req, res) => {
+router.post("/videos", (req, res) => {
     console.log('POST videos/ method called');
     let videos = videoData;
-    const {channel, description, id} = req.body;
+    const {channel, description} = req.body;
     console.log(req.body);
-    if (!channel || !description || !id) {
+   
+  
+	videos.push({
+		id: uuidv4(),
+		channel,
+		description,
+	});
+    
+    console.log(videos, "HELLO");
+    if (!channel || !description) {
         res.status(400).send("Please supply channel and description properties in your POST request");
     }
 
-    const newVideoObj = { id, channel, description };
-    videos.push(newVideoObj);
     fs.writeFile('./data/videos.json', 
     JSON.stringify(videos), (err) => {
         if (err) {
             res.status(500).send(err);
         }
         console.log("File written successfully!");
-        res.status(201).json(newVideoObj);
+        res.status(201).json(videos);
     })
 }) 
+
+
+// router.get('/', (req, res) => {
+//     axios
+//       .get(
+//         `https://project-2-api.herokuapp.com/videos?api_key=${process.env.BRAINFLIX_API_KEY}`
+//       )
+//       .then((resp) => {
+//         console.log(resp.data);
+//         res.json(resp.data);
+//       });
+//   });
+  
 
 module.exports = router;
